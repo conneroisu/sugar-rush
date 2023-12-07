4963c7b1e4e5b79c2c5f04b57928f7eb6ff634a8
conneroisu <conneroisu@outlook.com>
2023-12-07 12:09:19 -0600

    Add Sugar Rush ribbon icon handler

--- src/handlerRibbon.ts
+++ src/handlerRibbon.ts
@@ -1,9 +1,16 @@
 import { Notice } from "obsidian";
 import SugarRushPlugin from "./main";
 
+/**
+ * Represents a handler for the Sugar Rush ribbon.
+ */
 export default class SugarRushRibbonHandler {
 	plugin: SugarRushPlugin;
-	
+
+	/**
+	 * Creates an instance of SugarRushRibbonHandler.
+	 * @param plugin - The SugarRushPlugin instance.
+	 */
 	constructor(plugin: SugarRushPlugin) {
 		this.plugin = plugin;
 		if (this.plugin.settings.showRibbonIcon) {
@@ -11,11 +18,17 @@ export default class SugarRushRibbonHandler {
 		}
 	}
 
+	/**
+	 * Adds the Sugar Rush ribbon icon for rushing to a sugar view.
+	 **/
 	addRibbonIcon() {
-		this.plugin.addRibbonIcon("glass-water", "Sugar Rush", (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice("This is a notice!");
-		});
+		this.plugin.addRibbonIcon(
+			"glass-water",
+			"Sugar Rush",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				new Notice("This is a notice!");
+			}
+		);
 	}
-
 }
