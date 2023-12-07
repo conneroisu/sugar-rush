8844bd292bfd6192dbe5c2e4a48de38f9e5024f5
conneroisu <conneroisu@outlook.com>
2023-12-07 11:31:58 -0600

    Added Toggle Hidden Files Command

--- src/handlerCommands.ts
+++ src/handlerCommands.ts
@@ -18,5 +18,14 @@ export default class SugarRushCommandHandler {
 				open_sugar_file(this.plugin);
 			},
 		});
+
+		this.plugin.addCommand({
+			id: "toggle-hidden-files-in-sugar",
+			name: "Toggle Hidden Files In Sugar Views",
+			callback: () => {
+				this.plugin.settings.showHiddenFiles = !this.plugin.settings.showHiddenFiles;
+			},
+		});
+			
 	}
 }
