940be4c382a0c79a8e60c0859cfdd1f1152f73d3
conneroisu <conneroisu@outlook.com>
2023-12-10 12:56:35 -0600

    Added discriminated union to systemOperations

--- src/utils/systemOperations.ts
+++ src/utils/systemOperations.ts
@@ -1,4 +1,7 @@
 
+// Use a Discriminated Union to define the types of operations that can be performed on the file system
+type SystemOperation = CreateOperation | DeleteOperation | RenameOperation | MoveOperation;
+
 // Defines the operations that can be performed on the file system (e.g. create, delete, rename, and move)
 class CreateOperation {
 	constructor(public path: string) {}
