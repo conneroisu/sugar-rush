f7455b5ac2da1a8bfb96c1529bfc4f5eec259e04
conneroisu <conneroisu@outlook.com>
2023-12-13 15:41:57 -0600

    Working Icons associations and unknown file shown muist now change size of icons in index

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,55 @@
 import { type Extension } from "@codemirror/state";
 import { EditorView, ViewUpdate, gutter, lineNumbers, GutterMarker } from "@codemirror/view";
 import { Compartment, EditorState } from "@codemirror/state";
+import assets from "./assets/!index.json";
 
-let IconGutterCompartment = new Compartment();
+
+function getIconForFileExtension(extension: string): string {
+	const icon = assets["extension-associations"].find((association) => {
+		return association.extensions.includes(extension);
+	});
+	if (icon) {
+		return icon.data;
+	}
+	return "";
+}
 
 class Marker extends GutterMarker {
 	/** The text to render in gutter */
-	text: string;
+	extension: string;
 
 	constructor(text: string) {
 		super();
-		this.text = text;
+		this.extension = text;
 	}
 
 	toDOM() {
-		return document.createDiv(this.text + "a");
+		const icon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		icon.setAttrs({
+			width: "1em",
+			height: "1em",
+			viewBox: "0 0 1em 1em",
+			xmlns: "http://www.w3.org/2000/svg",
+			align: "center"
+		})
+		let innerHtml = getIconForFileExtension(this.extension);
+		if (innerHtml == "") {
+			innerHtml = getIconForFileExtension("");
+		}
+		icon.innerHTML = innerHtml;
+		return icon;
 	}
 }
 
-function relativeLineIcon(lineNo: number, state: EditorState) {
-	const charLength = 1;
-	const blank = " ".padStart(charLength, " ");
-
-	if (lineNo > state.doc.lines) { return blank; }
-
-	const extension = state.doc.line(lineNo).text.match(/(?<=\.)\w+$/);
-	
-	if (extension === null) {
-		return blank;
+const relativeLineIconGutter = gutter({
+	lineMarker: (view, line) => {
+		const ext = view.state.doc.line(view.state.doc.lineAt(line.from).number).text.match(/(?<=\.)\w+$/);
+		if (ext !== null) {
+			return new Marker(ext[0]);
+		}
+		return null;
 	}
-
-	return (extension).toString().padStart(charLength, " ");
-}
-
-// This shows the numbers in the gutter
-const showLineNumbers = IconGutterCompartment.of(
-	lineNumbers({ formatNumber: relativeLineIcon })
-);
+});
 
 // This ensures the numbers update
 // when selection (cursorActivity) happens
@@ -44,15 +57,11 @@ const lineNumbersUpdateListener = EditorView.updateListener.of(
 	(viewUpdate: ViewUpdate) => {
 		if (viewUpdate.docChanged) {
 			console.log("lineNumbersUpdateListener");
-			viewUpdate.view.dispatch({
-				effects: IconGutterCompartment.reconfigure(
-					lineNumbers({ formatNumber: relativeLineIcon })
-				),
-			});
 		}
 	}
 );
 
 export function iconGutter(): Extension {
-	return [showLineNumbers, lineNumbersUpdateListener];
+	return [lineNumbersUpdateListener, relativeLineIconGutter];
 }
+
