bdb301a9a9c6aa4d051b324b0e53672f2572af9a
conneroisu <conneroisu@outlook.com>
2023-12-19 18:25:56 -0600

    renamed extensionOperations and deleted pathVisibility

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { SugarRushSettingTab } from "./settings/settingsPage";
 import SugarRushCommandHandler from "./handlerCommands";
 import SugarRushRibbonHandler from "./handlerRibbon";
 import SugarRushFileSystemHandler from "./handlerFileSystem";
-import iconGutter from "./extensionIcons";
-import type { Extension } from "@codemirror/state";
 import SugarRushOperationHandler from "./handlerOperations";
 import SugarRushExtensionHandler from "./handlerExtensions";
+import iconGutter from "./extensionIcons";
+import type { Extension } from "@codemirror/state";
 
 export default class SugarRushPlugin extends Plugin {
 	settings!:			SugarRushPluginSettings;
@@ -22,21 +22,13 @@ export default class SugarRushPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
-		// Register the extension for `.sugar` files as markdown.
 		this.registerExtensions(["sugar"], "markdown");
-		// Add a new Sugar Rush Settings Tab.
 		this.addSettingTab(new SugarRushSettingTab(this));
-		// Add a new Sugar Rush Command Handler.
 		this.commandHandler = new SugarRushCommandHandler(this);
-		// Add a new Sugar Rush Ribbon Handler.
 		this.ribbonHandler = new SugarRushRibbonHandler(this);
-		// Add a new Sugar Rush File System Handler.
 		this.fileSystemHandler = new SugarRushFileSystemHandler(this);
-		// Add a new Sugar Rush Operation Handler.
 		this.operationHandler = new SugarRushOperationHandler(this);
-		// Add a new Sugar Rush Extension Handler.
 		this.extensionHandler = new SugarRushExtensionHandler(this);
-		// Register the extension for `.sugar` files as markdown.
 		this.registerEditorExtension(this.extension);
 		this.app.workspace.on("file-open", (file: TFile | null) => {
 			if (file && !this.fileSystemHandler.isSugarFile(file)) {
