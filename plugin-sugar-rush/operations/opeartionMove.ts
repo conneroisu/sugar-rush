fb33a82c0c53a6d0e019842727db839a7606566f
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-24 16:22:05 -0600

    Refactor MoveOperation class

--- plugin-sugar-rush/operations/opeartionMove.ts
+++ plugin-sugar-rush/operations/opeartionMove.ts
@@ -3,19 +3,25 @@ import type { AbstractOperation } from "./AbstractOperation";
 import { getIconForLineFileExtension } from "plugin-sugar-rush/extensions/extensionFormat";
 
 export class MoveOperation implements AbstractOperation {
+	plugin: SugarRushPlugin;
+	name: string = "Move";
+	id: string = "move";
+	description: string;
+	icon: string = getIconForLineFileExtension("move");
+	originalPath: string;
+	movedPath: string;
+
 	constructor(
 		plugin: SugarRushPlugin,
 		public path: string,
 		public newPath: string
 	) {
 		this.plugin = plugin;
+		this.description = `Would move ${this.path} to ${this.newPath}`;
+		this.originalPath = this.path;
+		this.movedPath = this.newPath;
 	}
-	plugin: SugarRushPlugin;
-	name: string = "Move";
-	description: string =
-		"Moves the file or directory at the given path to the new path";
-	icon: string = getIconForLineFileExtension("move");
-	id: string = "move";
+
 	run(): void {
 		throw new Error("Method not implemented.");
 	}
