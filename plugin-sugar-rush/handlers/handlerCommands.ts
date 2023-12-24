d159095b85784858fbb5e34ba96514e51dcf8446
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-24 17:43:05 -0600

    Refactor the SugarRushCommandHandler and SugarRushOperationHandler classes
    The attached git diff shows modifications to the `handlerCommands.ts` and `handlerOperations.ts` files in the `plugin-sugar-rush/handlers` directory. The changes involve the addition of class comments to both files.
    
    In `handlerCommands.ts`, a class comment describing the SugarRushCommandHandler is added. It explains that the handler is responsible for registering SugarRush commands to the SugarRushPlugin. Additionally, it notes that each new command needs to be manually added to this handler.
    
    In `handlerOperations.ts`, a class comment describing the SugarRushOperationHandler is added. It explains that the handler manages, organizes, and manipulates operations in the SugarRush Plugin environment. The comment also details the properties and methods of the handler class.

--- plugin-sugar-rush/handlers/handlerCommands.ts
+++ plugin-sugar-rush/handlers/handlerCommands.ts
@@ -6,9 +6,24 @@ import commandRefreshSugarView from "../commands/commandRefresh";
 
 import SugarRushPlugin from "../main";
 
+/**
+ * Class representing a handler for SugarRush commands.
+ * @class SugarRushCommandHandler
+ *
+ * This handler is responsible for registering SugarRush commands to the SugarRushPlugin.
+ * Note: Each new command is a unique implementation and needs to be manually added to this handler.
+ *
+ * @property {SugarRushPlugin} plugin - An instance of the SugarRushPlugin that this handler will register callbacks for.
+ *
+ * @constructor
+ * @param {SugarRushPlugin} plugin - An instance of SugarRushPlugin to attach the commands.
+ **/
 export default class SugarRushCommandHandler {
 	private readonly plugin: SugarRushPlugin;
 
+	/**
+	 * Create a command handler
+	 **/
 	constructor(plugin: SugarRushPlugin) {
 		this.plugin = plugin;
 		// add commands of the sugar-rush plugin
--- plugin-sugar-rush/handlers/handlerOperations.ts
+++ plugin-sugar-rush/handlers/handlerOperations.ts
@@ -1,6 +1,18 @@
 import type { TAbstractFile } from "obsidian";
 import type SugarRushPlugin from "../main";
 
+/**
+ * A handler class that manages, organizes and manipulates operations in the SugarRush Plugin environment.
+ * It adds, removes and pops off the operations in the operations' stack.
+ * @property {SugarRushPlugin} plugin - A read-only property defining the SugarRush plugin instance this handler is associated with
+ * @property {AbstractOperation[]} operations - An array holding the operations which this handler is currently managing
+ * @property {string[]} sugarReadings - An array holding the sugar readings
+ * @property {TAbstractFile[]} operationExceptions - An array storing encountered operation exceptions.
+ * @constructor {SugarRushOperationHandler} constructor - Constructs a new handler instance, taking a SugarRushPlugin instance as a required parameter
+ * @method {void} addOperation - Accepts an operation of type AbstractOperation, pushes it into the operations array
+ * @method {void} removeOperation - Accepts an operation of type AbstractOperation, removes it from the operations' array
+ * @method {void} popOperation - Pops off the last operation in the operations' stack and then runs that operation
+ **/
 export default class SugarRushOperationHandler {
 	private readonly  plugin: SugarRushPlugin;
 	operations: AbstractOperation[] = [];
@@ -22,8 +34,6 @@ export default class SugarRushOperationHandler {
 	popOperation() {
 		this.operations.pop()?.run();
 	}
-
-	
 }
 
 export abstract class AbstractOperation {
