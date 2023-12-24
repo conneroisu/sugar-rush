0b49d2920f36260e3e2b94cd55bbca27f9cce961
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-24 00:13:36 -0600

    added settingsPage test testing for its definition

---
+++ tests-sugar-rush/unit/settingsPage.test.ts
@@ -0,0 +1,11 @@
+
+import {SugarRushSettingTab} from "plugin-sugar-rush/views/settingsPage"
+import { describe, it, expect } from 'vitest';
+
+describe("SugarRushPluginSettings", () => {
+  it("should be defined", () => {
+  expect(SugarRushSettingTab).toBeDefined();
+  });
+});
+
+
