a0ff57102231b1448c99dc595b2f8d18e1af927b
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-24 17:00:48 -0600

    SugarRushExtensionHandler: Manage plugin configuration and extensions
    This commit adds a new class `SugarRushExtensionHandler` in the file `handlerExtensions.ts`, which is responsible for managing the configuration of SugarRush plugins and its associated extensions. The class has a `plugin` property to store the currently managed plugin and an `extensions` property to store the set of extensions associated with the plugin.
    
    The class constructor accepts a `SugarRushPlugin` object to initialize the `plugin` property. It also initializes the `extensions` property and registers this set of extensions to the plugin. The class provides a `clearExtensions()` method to clear all extensions and a `getExtensions()` method to update the `extensions` property based on the `showFileIcons` setting of the plugin and return the current set of extensions.
    
    Additionally, the constructor has been updated to include

--- plugin-sugar-rush/handlers/handlerExtensions.ts
+++ plugin-sugar-rush/handlers/handlerExtensions.ts
@@ -2,10 +2,32 @@ import type SugarRushPlugin from "./main";
 import { type Extension } from "@codemirror/state";
 import { formatGutter } from "./extensions/extensionFormat";
 
-export default class SugarRushExtensionHandler {
+ /**
+ * SugarRushExtensionHandler class manages the configuration of SugarRush plugins and its extensions.
+ *
+ * @property {SugarRushPlugin} plugin - Stores the plugin that is currently being managed.
+ * @property {Extension[]} extensions - Stores the set of extensions that are associated with the current plugin.
+ *
+ * @constructor
+ * @param {SugarRushPlugin} plugin - Accepts a SugarRushPlugin object to initialize the 'plugin' property.
+ * Upon instantiation, the constructor also initializes the 'extensions' property
+ * and registers this set of extensions to the plugin.
+ *
+ * @method clearExtensions() - Reinitializes the 'extensions' property to an empty array, effectively clearing all extensions.
+ *
+ * @method getExtensions() - Reinitializes and updates the 'extensions' property
+ * based on whether 'showFileIcons' setting is enabled for the plugin.
+ * Returns the current set of extensions.
+ */
+export
+ default class SugarRushExtensionHandler {
     private readonly  plugin: SugarRushPlugin;
 	extensions!: Extension[];
 	
+	/**
+	 * Creates a new extension handler for the Sugar Rush Plugin.
+	 * @param {SugarRushPlugin} plugin - Stores the plugin
+	 **/
 	constructor(plugin: SugarRushPlugin) {
 		this.plugin = plugin;
 		this.extensions = this.getExtensions();
