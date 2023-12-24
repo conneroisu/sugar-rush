baa54bd0a6176e9ead68098bb753254d4da432b3
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-23 22:13:58 -0600

    reduce unneeeded from handlerCommands

--- plugin-sugar-rush/handlerCommands.ts
+++ plugin-sugar-rush/handlerCommands.ts
@@ -1,4 +1,3 @@
-import { type Command } from "obsidian";
 import commandRushToSugarView from "./commands/commandRushToSugarView";
 import commandToggleHiddenFiles from "./commands/commandToggleHiddenFiles";
 import commandSelectSugarViewEntry from "./commands/commandSelectSugarViewEntry";
@@ -9,14 +8,9 @@ import SugarRushPlugin from "./main";
 
 export default class SugarRushCommandHandler {
 	private plugin: SugarRushPlugin;
-	commands: Command[] = [];
 
 	constructor(plugin: SugarRushPlugin) {
 		this.plugin = plugin;
-		this.addCommands();
-	}
-
-	async addCommands() {
 		this.plugin.addCommand(new commandRushToSugarView(this.plugin));
 		this.plugin.addCommand(new commandToggleHiddenFiles(this.plugin));
 		this.plugin.addCommand(new commandSelectSugarViewEntry(this.plugin));
