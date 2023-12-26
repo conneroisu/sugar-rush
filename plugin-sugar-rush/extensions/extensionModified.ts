6dae6bce92c1c3c639dffd6c5e27330a2016c045
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-25 20:39:30 -0600

    feat: Add modifiedGutter object for initializing a new gutter
    The `modifiedGutter` object is added to the `extensionModified.ts` file. It uses the `gutter` function from the `@codemirror/view` library to initialize a new gutter. The `gutter` function is called with an empty configuration object `{}`. The parameters for the gutter can be defined within this object.
    
    Add operations-related methods to SugarRushOperationHandler
    
    In the `handlerOperations.ts` file, several methods are added to the `SugarRushOperationHandler` class.
    
    - The `addOperation` method accepts an `AbstractOperation` object and adds it to the `operations` array.
    - The `removeOperation` method accepts an `AbstractOperation` object and removes it from the `operations` array.
    - The `popOperation` method pops off the last operation from the `operations` array and executes its `run` method if it exists.

--- plugin-sugar-rush/extensions/extensionModified.ts
+++ plugin-sugar-rush/extensions/extensionModified.ts
@@ -13,6 +13,14 @@ class ModifiedGutterMarker extends GutterMarker {
 	}
 }
 
+/**
+ * `modifiedGutter` is an object that uses the `gutter` function from "@codemirror/view" to initialize a new gutter.
+ * This gutter object is exported for use elsewhere within the application.
+ * Note that the `gutter` function is currently called with an empty configuration object `{}`.
+ * The parameters of your gutter can be defined within this object.
+ * TODO: implement
+ **/
+
 export const modifiedGutter = gutter({
 
 })
--- plugin-sugar-rush/handlers/handlerOperations.ts
+++ plugin-sugar-rush/handlers/handlerOperations.ts
@@ -19,18 +19,43 @@ export default class SugarRushOperationHandler {
 	sugarReadings: string[] = [];
 	operationExceptions: TAbstractFile[] = [];
 	
+	/**
+	 * Constructor for the SugarRushOperationHandler class.
+	 * Takes in a SugarRushPlugin instance and assigns it to the internal 'plugin' property of the class.
+	 * @param {SugarRushPlugin} plugin - An instance of SugarRushPlugin to associate with the SugarRushOperationHandler
+	 */
+
 	constructor(plugin: SugarRushPlugin) {
 		this.plugin = plugin;
 	}
 
+	/**
+	 * The addOperation method accepts an object of type AbstractOperation and adds it to the operations array defined in the SugarRushOperationHandler class.
+	 * @param {AbstractOperation} operation - The operation to add to the operations array
+	 * @returns {void}
+	 */
 	addOperation(operation: AbstractOperation) {
 		this.operations.push(operation);
 	}
+
+	/**
+	 * The removeOperation method accepts an object of type AbstractOperation and removes it from the operations array defined in the SugarRushOperationHandler class.
+	 * It does this by filtering out the operation to be removed from the current operations array
+	 * @param {AbstractOperation} operation - The operation to remove from the operations array
+	 * @returns {void}
+	 */
 
 	removeOperation(operation: AbstractOperation) {
 		this.operations = this.operations.filter((op) => op !== operation);
 	}
 
+	/**
+	 * The popOperation method is responsible for popping off the last operation from the operations' array, 
+	 * provided by the SugarRushOperationHandler class.
+	 * If there exists an operation to be popped, the 'run' method of this operation is executed immediately after it's detachment.
+	 * @returns {void}
+	 */
+	
 	popOperation() {
 		this.operations.pop()?.run();
 	}
