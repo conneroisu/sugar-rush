de572cf184b270114a04e4ddb53a3294b4f7d520
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-25 20:50:11 -0600

    feat: Add documentation and refactor code in SugarRushPlugin
    The `SugarRushPlugin` class has been updated to include documentation comments describing its attributes and methods. The `onload`, `onunload`, `loadSettings`, and `saveSettings` methods have also been refactored to include documentation comments explaining their functionality.
    
    These changes improve the readability and maintainability of the codebase.

--- plugin-sugar-rush/main.ts
+++ plugin-sugar-rush/main.ts
@@ -6,13 +6,46 @@ import SugarRushFileSystemHandler from "./handlers/handlerFileSystem";
 import SugarRushOperationHandler from "./handlers/handlerOperations";
 import SugarRushExtensionHandler from "./handlers/handlerExtensions";
 
-export default class SugarRushPlugin extends Plugin {
+export default /**
+ * The `SugarRushPlugin` class extends the `Plugin` base class to provide functionality related to handling sugar files in the Obsidian app.
+ *
+ * Attributes:
+ * - `settings` : instance of SugarRushPluginSettings holds the settings for the plugin.
+ * - `commandHandler` : instance of SugarRushCommandHandler manages command related operations for the plugin.
+ * - `fileSystemHandler` : instance of SugarRushFileSystemHandler manages filesystem related operations for sugar files.
+ * - `operationHandler` : instance of SugarRushOperationHandler manages operations for the plugin.
+ * - `extensionHandler` : instance of SugarRushExtensionHandler manages extension related operations for the plugin.
+ * - `app` : instance of the App class from Obsidian API.
+ *
+ * Methods:
+ * - `onload`: This method is run when the plugin is loaded. It initialization events for extensions, settings tab, and handlers.
+ * - `onunload`: This method is run when the plugin is unloaded. It deletes all sugar files with the help of file system handler.
+ * - `loadSettings`: This method loads the settings for the plugin from the storage and merges it with default settings.
+ * - `saveSettings`: This method saves the settings for the plugin to the storage.
+ */
+
+class SugarRushPlugin extends Plugin {
 	settings!: SugarRushPluginSettings;
 	commandHandler!: SugarRushCommandHandler;
 	fileSystemHandler!: SugarRushFileSystemHandler;
 	operationHandler!: SugarRushOperationHandler;
 	extensionHandler!: SugarRushExtensionHandler;
 	app!: App;
+
+	/**
+	 * onLoad Method:
+	 * This method is invoked async when the plugin is loaded to the workspace. It performs several initialization tasks:
+	 * - Load settings using the loadSettings method.
+	 * - Delete all Sugar Files in the file system.
+	 * - Register the sugar file extension as markdown.
+	 * - Add a new settings tab with a SugarRushSettingsView instance.
+	 * - Instantiate commandHandler, fileSystemHandler, operationHandler, and extensionHandler.
+	 * - Attach an event handler to the workspace to handle file-opening events. If the opened file is not a sugar file,
+	 *   update the workspace options and get sugar file extensions. If it's a sugar file and there are operations in the
+	 *   queue, open the SugarOperationView modal.
+	 *
+	 * Please note that this method doesn't take any arguments and doesn't return anything.
+	 */
 
 	async onload() {
 		await this.loadSettings();
@@ -36,6 +69,13 @@ export default class SugarRushPlugin extends Plugin {
 		});
 	}
 
+	/**
+	* onunload Method:
+	* This method is invoked when the plugin is unloaded from the workspace. Its function is to delete all Sugar Files from the file
+	* system. This is achieved by calling the deleteAllSugarFiles method of the fileSystemHandler.
+	*
+	* Please note that this method doesn't take any arguments and doesn't return anything.
+	*/
 	onunload() { this.fileSystemHandler.deleteAllSugarFiles(); }
 
 	async loadSettings() {
@@ -46,6 +86,15 @@ export default class SugarRushPlugin extends Plugin {
 		);
 	}
 
+	/**
+	 * saveSettings Method:
+	 *
+	 * This asynchronous method is responsible for saving the settings of the SugarRushPlugin. It takes no parameters.
+	 * It uses the saveData method to save the current state of settings into the storage.
+	 *
+	 * Please note that the method does not return anything.
+	 */
+	
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
--- plugin-sugar-rush/settings.ts
+++ plugin-sugar-rush/settings.ts
@@ -1,10 +1,44 @@
-export const DEFAULT_SETTINGS: SugarRushPluginSettings = {
+export const /**
+ * `DEFAULT_SETTINGS` is a constant object that defines the default settings for the SugarRush plugin.
+ * Each property corresponds to a configuration setting.
+ * 
+ * @const DEFAULT_SETTINGS : SugarRushPluginSettings
+ * 
+ * @property {boolean} debug - Default value for debug mode is 'false'.
+ *
+ * @property {boolean} showHiddenFiles - Default value to show hidden files in the file explorer is 'true'.
+ *
+ * @property {boolean} showFileIcons - Default value to show file icons in the file explorer is 'true'.
+ * 
+ * @property {boolean} showFileSizes - Default value to show file sizes in the file explorer is 'false'.
+ * 
+ * @property {boolean} showModifiedTimes - Default value to show file modification dates/times in the
+ * file explorer is 'false'.
+ */
+
+DEFAULT_SETTINGS: SugarRushPluginSettings = {
 	debug: false,
 	showHiddenFiles: true,
 	showFileIcons: true,
 	showFileSizes: false,
 	showModifiedTimes: false,
 };
+
+/**
+ * An interface representing the settings for the SugarRush plugin.
+ *
+ * @interface SugarRushPluginSettings
+ *
+ * @property {boolean} debug - Represents whether debug mode is activated or not.
+ *
+ * @property {boolean} showHiddenFiles - Represents whether to show hidden files in the file explorer or not.
+ *
+ * @property {boolean} showFileIcons - Represents whether to show file icons in the file explorer or not.
+ *
+ * @property {boolean} showFileSizes - Represents whether to show file sizes in the file explorer or not.
+ *
+ * @property {boolean} showModifiedTimes - Represents whether to show file modification dates/times in the file explorer or not.
+ */
 
 export interface SugarRushPluginSettings {
 	debug: boolean;
