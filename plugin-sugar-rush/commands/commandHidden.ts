c300edb56dce0c3f848e76f6558179a57a0f1ad2
connero <88785126+conneroisu@users.noreply.github.com>
2023-12-28 00:32:34 -0600

    latest

--- plugin-sugar-rush/commands/commandHidden.ts
+++ plugin-sugar-rush/commands/commandHidden.ts
@@ -1,7 +1,14 @@
 import { Notice } from "obsidian";
 import type SugarRushPlugin from "plugin-sugar-rush/main";
 
-export default function commandToggleHiddenFiles(plugin: SugarRushPlugin, checking: boolean) {
+/**
+ * Toggles the display of hidden files in the file explorer.
+ * @param {SugarRushPlugin} plugin - An instance of SugarRushPlugin.
+ * @param {boolean} checking - If true, then we are checking if the command can be run.
+ * @returns {boolean} - Returns true if the command can be run.
+ **/
+export default function commandToggleHiddenFiles(plugin: SugarRushPlugin, checking: boolean): boolean {
+	// If checking is true, then we are checking if the command can be run.
 	if (checking) {
 		const activeFile = plugin.app.workspace.getActiveFile();
 		if (!activeFile) { return false; }
@@ -15,4 +22,5 @@ export default function commandToggleHiddenFiles(plugin: SugarRushPlugin, checki
 	}
 	plugin.settings.showHiddenFiles = !plugin.settings.showHiddenFiles;
 	plugin.saveSettings();
+	return true;
 }
