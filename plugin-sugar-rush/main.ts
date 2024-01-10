8626b24d6b281bcbd0a863fd86aa65d2c8fd34c8
connero <88785126+conneroisu@users.noreply.github.com>
2024-01-09 21:43:03 -0600

    Delete SugarRushCommandHandler class and move its methods as individual commands to the main.ts file.

--- plugin-sugar-rush/main.ts
+++ plugin-sugar-rush/main.ts
@@ -1,6 +1,13 @@
-import { App, Plugin, TFile } from "obsidian";
+import {
+	App,
+	Editor,
+	Notice,
+	Plugin,
+	TAbstractFile,
+	TFile,
+	TFolder,
+} from "obsidian";
 import SugarRushExtensionHandler from "./handlerExtensions";
-import SugarRushCommandHandler from "./handlerCommands";
 import SugarRushFileSystemHandler from "./handlerFileSystem";
 import { DEFAULT_SETTINGS, type SugarRushPluginSettings } from "./settings";
 import { SugarRushOperationView } from "./viewOperation";
@@ -25,7 +32,6 @@ import { SugarRushSettingView } from "./viewSettings";
  **/
 export default class SugarRushPlugin extends Plugin {
 	settings!: SugarRushPluginSettings;
-	commandHandler!: SugarRushCommandHandler;
 	fileSystemHandler!: SugarRushFileSystemHandler;
 	extensionHandler!: SugarRushExtensionHandler;
 	app!: App;
@@ -37,7 +43,7 @@ export default class SugarRushPlugin extends Plugin {
 		await this.loadSettings();
 		this.registerExtensions(["sugar"], "markdown");
 		this.addSettingTab(new SugarRushSettingView(this));
-		this.commandHandler = new SugarRushCommandHandler(this);
+		this.addCommands();
 		this.fileSystemHandler = new SugarRushFileSystemHandler(this);
 		this.extensionHandler = new SugarRushExtensionHandler(this);
 		this.app.workspace.on("file-open", (file: TFile | null) => {
@@ -57,6 +63,187 @@ export default class SugarRushPlugin extends Plugin {
 	 **/
 	onunload() {
 		this.fileSystemHandler.removeAllSugarFiles();
+	}
+
+	addCommands() {
+		this.addCommand({
+			id: "rush-to-sugar-view",
+			name: "Rush to Sugar View",
+			editorCheckCallback: (checking: boolean) => {
+				const activeFile = this.app.workspace.getActiveFile();
+				if (checking) {
+					if (activeFile) {
+						return true;
+					}
+					return false;
+				}
+				const leaf = this.app.workspace.getMostRecentLeaf();
+				if (activeFile && leaf) {
+					const sugarFilePath =
+						this.fileSystemHandler.getSugarFilePath(activeFile);
+					if (activeFile.parent && activeFile.parent.name !== "") {
+						const file: TFile | null | undefined | TAbstractFile =
+							this.app.vault.getAbstractFileByPath(sugarFilePath);
+						if (!file) {
+							this.fileSystemHandler
+								.createSugarFile(activeFile)
+								.then((file: TFile) => {
+									this.fileSystemHandler.loadFile(file, leaf);
+								});
+						} else {
+							if (file instanceof TFile) {
+								this.fileSystemHandler.loadFile(file, leaf);
+							}
+						}
+					} else {
+						const file: TFile | null | undefined | TAbstractFile =
+							this.app.vault.getAbstractFileByPath(sugarFilePath);
+						if (!file) {
+							this.fileSystemHandler
+								.createSugarFile(activeFile)
+								.then((file: TFile) => {
+									this.fileSystemHandler.loadFile(file, leaf);
+								});
+						} else {
+							if (file instanceof TFile) {
+								this.fileSystemHandler.loadFile(file, leaf);
+							}
+						}
+					}
+				} else {
+					if (activeFile && leaf) {
+						return true;
+					}
+				}
+				return true;
+			},
+		});
+		this.addCommand({
+			id: "toggle-hidden-files",
+			name: "Toggle Hidden Files",
+			editorCheckCallback: (checking: boolean) => {
+				if (checking) {
+					const activeFile = this.app.workspace.getActiveFile();
+					if (!activeFile) {
+						return false;
+					}
+					if (activeFile.extension !== "sugar") {
+						return false;
+					}
+					return true;
+				}
+				if (this.fileSystemHandler.operationsMap.size > 0) {
+					new Notice(
+						"Cannot toggle hidden files while an operation is in progress."
+					);
+				}
+				this.settings.showHiddenFiles = !this.settings.showHiddenFiles;
+				this.saveSettings();
+				return true;
+			},
+		});
+		this.addCommand({
+			id: "select-sugar-view-entry",
+			name: "Select Sugar View Entry",
+			editorCheckCallback: (checking: boolean, editor: Editor) => {
+				const activeFile = this.app.workspace.getActiveFile();
+				const leaf = this.app.workspace.getMostRecentLeaf();
+				if (checking) {
+					return true;
+				}
+				if (activeFile && activeFile.extension === "sugar") {
+					const id = editor
+						.getLine(editor.getCursor().line)
+						.split("<a href=")[1]
+						.split(">")[0];
+					const file = this.fileSystemHandler.abstractMap.get(
+						parseInt(id)
+					);
+					if (!file || !leaf) {
+						return false;
+					}
+					if (file instanceof TFile) {
+						if (this.settings.debug) {
+							console.log(
+								`commandSelectSugarViewEntry.ts: selecting and opening TFile: ${file.path}`
+							);
+						}
+						this.fileSystemHandler.loadFile(file, leaf);
+					}
+					if (file instanceof TFolder) {
+						if (this.settings.debug) {
+							console.log(
+								`commandSelectSugarViewEntry.ts: selecting and opening TFolder: ${file.path}`
+							);
+						}
+					}
+
+					return true;
+				}
+				if (this.settings.debug) {
+					console.log(
+						"commandSelectSugarViewEntry.ts: not selecting anything | checking is "
+					);
+				}
+				return false;
+			},
+		});
+		this.addCommand({
+			id: "save-sugar-view",
+			name: "Save Sugar View",
+			editorCheckCallback: (checking: boolean) => {
+				const activeFile = this.app.workspace.getActiveFile();
+				const leaf = this.app.workspace.getMostRecentLeaf();
+				if (!checking && activeFile && leaf) {
+					this.app.vault
+						.modify(
+							activeFile,
+							this.fileSystemHandler.generateSugarFileContent(
+								activeFile
+							)
+						)
+						.then(() => {
+							if (this.settings.debug) {
+								console.log("Refreshed file", activeFile);
+							}
+							this.fileSystemHandler.loadFile(activeFile, leaf);
+						});
+				} else {
+					if (activeFile && leaf) {
+						return true;
+					}
+				}
+				return true;
+			},
+		});
+		this.addCommand({
+			id: "refresh-sugar-view",
+			name: "Refresh Sugar View",
+			editorCheckCallback: (checking: boolean) => {
+				const activeFile = this.app.workspace.getActiveFile();
+				const leaf = this.app.workspace.getMostRecentLeaf();
+				if (!checking && activeFile && leaf) {
+					this.app.vault
+						.modify(
+							activeFile,
+							this.fileSystemHandler.generateSugarFileContent(
+								activeFile
+							)
+						)
+						.then(() => {
+							if (this.settings.debug) {
+								console.log("Refreshed file", activeFile);
+							}
+							this.fileSystemHandler.loadFile(activeFile, leaf);
+						});
+				} else {
+					if (activeFile && leaf) {
+						return true;
+					}
+				}
+				return true;
+			},
+		});
 	}
 
 	/**
