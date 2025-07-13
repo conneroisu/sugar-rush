87239dab9b537209a8e8a124a410c13779f8078b
connerohnesorge <conneroisu@outlook.com>
2025-07-13 07:59:43 -0500

    latest

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, type MarkdownFileInfo } from 'obsidian';
 
 interface MyPluginSettings {
   mySetting: string;
@@ -11,18 +9,10 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 }
 
 export default class MyPlugin extends Plugin {
-  settings: MyPluginSettings;
+  settings!: MyPluginSettings;
 
   async onload() {
     await this.loadSettings();
-
-    // This creates an icon in the left ribbon.
-    const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-      // Called when the user clicks the icon.
-      new Notice('This is a notice!');
-    });
-    // Perform additional things with the ribbon
-    ribbonIconEl.addClass('my-plugin-ribbon-class');
 
     // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
     const statusBarItemEl = this.addStatusBarItem();
@@ -40,7 +30,7 @@ export default class MyPlugin extends Plugin {
     this.addCommand({
       id: 'sample-editor-command',
       name: 'Sample editor command',
-      editorCallback: (editor: Editor, view: MarkdownView) => {
+      editorCallback: (editor: Editor, view: MarkdownView | MarkdownFileInfo) => {
         console.log(editor.getSelection());
         editor.replaceSelection('Sample Editor Command');
       }
@@ -67,15 +57,6 @@ export default class MyPlugin extends Plugin {
 
     // This adds a settings tab so the user can configure various aspects of the plugin
     this.addSettingTab(new SampleSettingTab(this.app, this));
-
-    // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-    // Using this function will automatically remove the event listener when this plugin is disabled.
-    this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-      console.log('click', evt);
-    });
-
-    // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-    this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
   }
 
   onunload() {
@@ -119,6 +100,7 @@ class SampleSettingTab extends PluginSettingTab {
     const { containerEl } = this;
 
     containerEl.empty();
+
 
     new Setting(containerEl)
       .setName('Setting #1')
